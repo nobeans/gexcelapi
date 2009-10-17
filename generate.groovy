8ad25cb7e5050659120f92ab5a43fa2548eb7b58
nobeans <nobeans@gmail.com>
2009-10-17 23:27:08 +0900

    removed all comments

--- generate.groovy
+++ generate.groovy
@@ -1,15 +1,7 @@
-// usage: groovy generate.groovy entity-def.xls
-
-// ----------------------------
-// Grapeによるライブラリ取得
 @Grab(group='org.apache.poi', module='poi', version='3.5-beta3')
 import org.apache.poi.hssf.usermodel.*
 import org.apache.poi.poifs.filesystem.*
-//groovy.grape.Grape.grab(group:'org.apache.poi', module:'poi', version:'3.0.2-FINAL')
-//@Grab('org.apache.poi:poi:3.0.2-FINAL') // only for Groovy1.7
 
-// ----------------------------
-// エンティティ情報保持用のクラス
 class Table {
     String physicalName = ""
     String logicalName = ""
@@ -31,43 +23,32 @@ class Column {
     }
 }
 
-// ----------------------------
-// Excelファイルの読み込み
 def inputFile = new File(args[0])
 def book = new HSSFWorkbook(new POIFSFileSystem(new FileInputStream(inputFile)))
-def sheet = book.getSheetAt(0) // 今回は1番目のシートのみ対象とする
+def sheet = book.getSheetAt(0) 
 def cell = { row, col ->
     sheet.getRow(row)?.getCell((short)col)
 }
 
-// ----------------------------
-// エンティティ情報を取得する
-//    今回は必要な情報のセル座標はgivenなものとする(キーで探索などはしない)
 def table = new Table(
     physicalName: cell(2,0).stringCellValue,
     logicalName: cell(2,1).stringCellValue
 )
 for (i in 6..100) {
-    // physicalNameが空の場合はそこで打ち切る
+    
     if (!cell(i, 0)?.stringCellValue) break
 
     table.columns << new Column(
         physicalName: cell(i, 0)?.stringCellValue,
         logicalName:  cell(i, 1)?.stringCellValue,
         type:         cell(i, 2)?.stringCellValue.toUpperCase(),
-        length:       cell(i, 3)?.numericCellValue.intValue(), // Double -> int
+        length:       cell(i, 3)?.numericCellValue.intValue(), 
         notNull:      cell(i, 4)?.booleanCellValue
     )
 }
-//println table
 
-// ----------------------------
-// ソースファイルを出力する
-
-// 先頭を大文字化するクロージャ(メソッドでも良いが、groovyっぽくクロージャで)
 def capitalize = { text -> text[0].toUpperCase() + text.substring(1) }
 
-// カラム情報からフィールド・メソッド情報を生成する
 def imports = []
 def fields = []
 def methods = []
@@ -75,7 +56,7 @@ table.columns.each { column ->
     def propertyName = column.physicalName
     def capitalizedName = capitalize(propertyName)
 
-    // typeごとに実装(現状は、今使用中の方のみ対応している)
+    
     def javaType
     if (column.type == "VCHAR") {
         javaType = "String"
@@ -90,7 +71,6 @@ table.columns.each { column ->
     methods << "public void set$capitalizedName($javaType $propertyName) { this.$propertyName = $propertyName; }"
 }
 
-// 出力する
 def indent = "    "
 def sep = System.getProperty('line.separator')
 def className = table.physicalName
--- read.groovy
+++ read.groovy
@@ -1,46 +1,33 @@
-// usage: groovy read.groovy read.xls
-
-// ----------------------------
-// Grapeによるライブラリ取得
 @Grab(group='org.apache.poi', module='poi', version='3.5-beta3')
 import org.apache.poi.hssf.usermodel.*
 import org.apache.poi.poifs.filesystem.*
-//groovy.grape.Grape.grab(group:'org.apache.poi', module:'poi', version:'3.0.2-FINAL')
-//@Grab('org.apache.poi:poi:3.0.2-FINAL') // only for Groovy1.7
 
-// ----------------------------
-// Excelファイルの読み込み
 def inputFile = new File(args[0])
 def book = new HSSFWorkbook(new POIFSFileSystem(new FileInputStream(inputFile)))
-def sheet1 = book.getSheetAt(0) // 第1シート
-def sheet2 = book.getSheetAt(1) // 第2シート
-def sheet3 = book.getSheet("Sheet3") // シート名で指定も可能
+def sheet1 = book.getSheetAt(0) 
+def sheet2 = book.getSheetAt(1) 
+def sheet3 = book.getSheet("Sheet3") 
 
-def sheet = sheet1 // 以降で使うシートを選択
-//def sheet = sheet2
-//def sheet = sheet3
+def sheet = sheet1 
 
-// ----------------------------
-// セルの参照
-println sheet.getRow(0)?.getCell((short) 0).stringCellValue // A1
-println sheet.getRow(1)?.getCell((short) 0).stringCellValue // A2
-println sheet.getRow(2)?.getCell((short) 0).numericCellValue.intValue() // A3 (double->int)
-println sheet.getRow(3)?.getCell((short) 0).dateCellValue // A4
-println sheet.getRow(4)?.getCell((short) 0).booleanCellValue // A5
-println sheet.getRow(5)?.getCell((short) 0).booleanCellValue // A6
-println sheet.getRow(0)?.getCell((short) 0).stringCellValue // B1
-println sheet.getRow(0)?.getCell((short) 1).stringCellValue // B2
+println sheet.getRow(0)?.getCell((short) 0).stringCellValue 
+println sheet.getRow(1)?.getCell((short) 0).stringCellValue 
+println sheet.getRow(2)?.getCell((short) 0).numericCellValue.intValue() 
+println sheet.getRow(3)?.getCell((short) 0).dateCellValue 
+println sheet.getRow(4)?.getCell((short) 0).booleanCellValue 
+println sheet.getRow(5)?.getCell((short) 0).booleanCellValue 
+println sheet.getRow(0)?.getCell((short) 0).stringCellValue 
+println sheet.getRow(0)?.getCell((short) 1).stringCellValue 
 
-// （特に意味はないが）groovy風にクロージャで定義してみる
 def cell = { row, col ->
     sheet.getRow(row)?.getCell((short) col)
 }
-println cell(0, 0).stringCellValue // A1
-println cell(1, 0).stringCellValue // A2
-println cell(2, 0).numericCellValue.intValue() // A3 (double->int)
-println cell(3, 0).dateCellValue // A4
-println cell(4, 0).booleanCellValue // A5
-println cell(5, 0).booleanCellValue // A6
-println cell(0, 0).stringCellValue // B1
-println cell(0, 1).stringCellValue // B2
+println cell(0, 0).stringCellValue 
+println cell(1, 0).stringCellValue 
+println cell(2, 0).numericCellValue.intValue() 
+println cell(3, 0).dateCellValue 
+println cell(4, 0).booleanCellValue 
+println cell(5, 0).booleanCellValue 
+println cell(0, 0).stringCellValue 
+println cell(0, 1).stringCellValue 
 
