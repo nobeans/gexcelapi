1018ef33fae53551b98cb5f469ab0addc633332b
nobeans <nobeans@gmail.com>
2009-10-18 01:40:59 +0900

    code of proofing of the idea

--- read.groovy
+++ read.groovy
@@ -2,32 +2,84 @@
 import org.apache.poi.hssf.usermodel.*
 import org.apache.poi.poifs.filesystem.*
 
-def inputFile = new File(args[0])
-def book = new HSSFWorkbook(new POIFSFileSystem(new FileInputStream(inputFile)))
-def sheet1 = book.getSheetAt(0) 
-def sheet2 = book.getSheetAt(1) 
-def sheet3 = book.getSheet("Sheet3") 
-
-def sheet = sheet1 
-
-println sheet.getRow(0)?.getCell((short) 0).stringCellValue 
-println sheet.getRow(1)?.getCell((short) 0).stringCellValue 
-println sheet.getRow(2)?.getCell((short) 0).numericCellValue.intValue() 
-println sheet.getRow(3)?.getCell((short) 0).dateCellValue 
-println sheet.getRow(4)?.getCell((short) 0).booleanCellValue 
-println sheet.getRow(5)?.getCell((short) 0).booleanCellValue 
-println sheet.getRow(0)?.getCell((short) 0).stringCellValue 
-println sheet.getRow(0)?.getCell((short) 1).stringCellValue 
-
-def cell = { row, col ->
-    sheet.getRow(row)?.getCell((short) col)
+//---------------------------------------------------------
+def ascii2num = { ascii ->
+    def num = 0
+    ascii.reverse().toUpperCase().toCharArray().eachWithIndex { ch, i ->
+        final maxDelta = 25
+        def deltaFromA = (ch as int) - 65
+        num += deltaFromA * (i*maxDelta)
+    }
+    num
 }
-println cell(0, 0).stringCellValue 
-println cell(1, 0).stringCellValue 
-println cell(2, 0).numericCellValue.intValue() 
-println cell(3, 0).dateCellValue 
-println cell(4, 0).booleanCellValue 
-println cell(5, 0).booleanCellValue 
-println cell(0, 0).stringCellValue 
-println cell(0, 1).stringCellValue 
+
+// "A8" -> 0
+def colIndex = { expr ->
+    def matcher = (expr =~ /([a-zA-Z]+)([0-9]+)/)
+    println matcher[0][1]
+    println ascii2num(matcher[0][1])
+    ascii2num(matcher[0][1])
+}
+// "A8" -> 7
+def rowIndex = { expr ->
+    def matcher = (expr =~ /([a-zA-Z]+)([0-9]+)/)
+    (matcher[0][2] as int) - 1
+}
+
+// staticではdefineが使えない(!?)ので。
+HSSFWorkbook.metaClass.static.load = { String file -> load(new File(file)) }
+HSSFWorkbook.metaClass.static.load = { File file -> load(new FileInputStream(file)) }
+HSSFWorkbook.metaClass.static.load = { InputStream is -> new HSSFWorkbook(new POIFSFileSystem(is)) }
+
+HSSFWorkbook.metaClass.define {
+    getAt { idx -> delegate.getSheetAt(idx) }
+    getProperty { name -> delegate.getSheet(name) }
+}
+HSSFSheet.metaClass.define {
+    getProperty { name -> delegate.getRow(rowIndex(name))?.getCell(colIndex(name)) }
+    setProperty { name, value -> delegate.getRow(rowIndex(name))?.getCell(colIndex(name)).setCellValue(value) }
+}
+HSSFCell.metaClass.define {
+    asInt { delegate.numericCellValue.intValue() }
+    asDate { delegate.dateCellValue }
+    asBoolean { delegate.booleanCellValue }
+}
+class GExcel {
+    static load(String file) { load(new File(file)) }
+    static load(File file) { load(new FileInputStream(file)) }
+    static load(InputStream is) { new HSSFWorkbook(new POIFSFileSystem(is)) }
+}
+//---------------------------------------------------------
+
+//def book = HSSFWorkbook.load(args[0])
+def book = GExcel.load(args[0])
+
+def sheet = book[0]
+
+println sheet["A1"]
+
+println sheet.A1
+sheet.A1 = "MODIFIED"
+println sheet.A1
+
+println sheet.A2
+println sheet.AA1
+println sheet.BC1
+println sheet.ZZ1
+println sheet.ZZZ1
+
+//println sheet[A1..B5]
+//sheet[A1..B5].filledBy 0
+//println sheet[A1..B5]
+
+def sheet3 = book.Sheet3 // by sheet name
+
+println sheet3.A1
+println sheet3.A2
+println sheet3.A3.asInt()
+println sheet3.A4.asDate()
+println sheet3.A5.asBoolean()
+println sheet3.A6.asBoolean()
+println sheet3.B1
+println sheet3.B2
 
