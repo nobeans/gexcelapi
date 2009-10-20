f67f6aa9414673e76140f201b78017babb62005c
nobeans <nobeans@gmail.com>
2009-10-21 00:12:07 +0900

    restructured methods of accessing cell
    - implicitly accessing value by value property
    - accessing value by asType notation
    - both has tests
    - fixed boolean cell value (but revealed some warnings when run)

--- GExcel.groovy
+++ GExcel.groovy
@@ -1,12 +1,13 @@
 @Grab(group='org.apache.poi', module='poi', version='3.5-beta3')
 import org.apache.poi.hssf.usermodel.*
 import org.apache.poi.poifs.filesystem.*
+import org.apache.poi.ss.usermodel.*
 
 class GExcel {
     static { setupMetaClass() }
 
     private static setupMetaClass() {
-        // staticではdefineが使えない(!?)ので。
+        // maybe cannot use "define" for static methods
         HSSFWorkbook.metaClass.static.load = { String file -> load(new File(file)) }
         HSSFWorkbook.metaClass.static.load = { File file -> load(new FileInputStream(file)) }
         HSSFWorkbook.metaClass.static.load = { InputStream is -> new HSSFWorkbook(new POIFSFileSystem(is)) }
@@ -20,16 +21,25 @@ class GExcel {
             setProperty { name, value -> delegate.getRow(rowIndex(name))?.getCell(colIndex(name)).setCellValue(value) }
         }
         HSSFCell.metaClass.define {
-            asInt { delegate.numericCellValue.intValue() }
-            asDate { delegate.dateCellValue }
-            asBoolean { delegate.booleanCellValue }
-            asString { delegate.stringCellValue }
+            getValue {
+                // implicitly accessing value by appropriate type
+                switch(delegate.cellType) {
+                    case Cell.CELL_TYPE_STRING:  return delegate.stringCellValue
+                    case Cell.CELL_TYPE_NUMERIC: return delegate.numericCellValue
+                    case Cell.CELL_TYPE_BOOLEAN: return delegate.booleanCellValue
+                    default: throw new RuntimeException("unsupported cell type: ${delegate.cellType}")
+                }
+            }
+            setValue { value -> delegate.setCellValue(value) }
+            leftShift { value -> delegate.setCellValue(value) }
             asType { Class type ->
                 switch(type) {
-                    case Integer: return delegate.asInt()
-                    case Boolean: return delegate.asBoolean()
-                    case Date:    return delegate.asDate()
-                    case String:  return delegate.asString()
+                    case Double:  return delegate.numericCellValue
+                    case Integer: return delegate.numericCellValue.intValue()
+                    case Boolean: return delegate.booleanCellValue
+                    case Date:    return delegate.dateCellValue
+                    case String:  return delegate.stringCellValue
+                    default: throw new RuntimeException("unsupported cell type: ${delegate.cellType}")
                 }
             }
         }
--- GExcelTest.groovy
+++ GExcelTest.groovy
@@ -27,4 +27,89 @@ class GExcelTest extends GroovyTestCase {
         assert GExcel.colIndex("ZZ88") == 701
         assert GExcel.colIndex("AAA88") == 702
     }
+
+    void testLoad() throws Exception {
+        assert GExcel.load("./sample.xls")
+        assert GExcel.load(new File("sample.xls"))
+        new File("sample.xls").withInputStream { is ->
+            assert GExcel.load(is)
+        }
+    }
+
+    void testAccessSheet() throws Exception {
+        def book = GExcel.load(new File("sample.xls"))
+        def sheet = book[0]
+        assert sheet != book[1]
+
+        def sheet3 = book.Sheet3 // by sheet name
+        assert sheet3 == book[2]
+        assert sheet3 == book["Sheet3"]
+    }
+
+    void testAccessCellValue() throws Exception {
+        def book = GExcel.load(new File("sample.xls"))
+        def sheet = book[0]
+        assert sheet["A1"].value == "Sheet1-A1"
+ 
+        assert sheet.A1.value == "Sheet1-A1"
+        sheet.A1.value = "MODIFIED"
+        assert sheet.A1.value == "MODIFIED"
+        sheet.A1 << "MORE-MODIFIED"
+        assert sheet.A1.value == "MORE-MODIFIED"
+
+        assert sheet.B1.value == "B1の内容"
+        assert sheet.B2.value == "B2の内容"
+    }
+
+    void testAccessCellsOfSomeTypes() throws Exception {
+        def book = GExcel.load(new File("sample.xls"))
+        def sheet = book[0]
+
+        assert sheet.A1.value == "Sheet1-A1"
+        assert sheet.A1.value in String
+
+        assert sheet.A2.value == "あいうえお"
+        assert sheet.A2.value in String
+
+        assert sheet.A3.value == 1234.0
+        assert sheet.A3.value in Double
+        assert sheet.A3.value.intValue() in Integer // method to get int value
+
+        // it cannot implicitly accessing value, so explicitly access.
+        assert sheet.A4.dateCellValue in java.util.Date
+        assert sheet.A4.dateCellValue as String == "Thu Oct 22 00:00:00 JST 2009"
+
+        assert sheet.A5.value == true
+        assert sheet.A6.value == false
+    }
+
+    void testAccessCellsOfSomeTypes_asType() throws Exception {
+        def book = GExcel.load(new File("sample.xls"))
+        def sheet = book[0]
+
+        assert (sheet.A1 as String) == "Sheet1-A1"
+        assert (sheet.A1 as String) in String
+
+        assert (sheet.A2 as String)  == "あいうえお"
+        assert (sheet.A2 as String) in String
+
+        assert (sheet.A3 as Double) == 1234.0
+        assert (sheet.A3 as Double) in Double
+        assert (sheet.A3 as Integer) == 1234
+        assert (sheet.A3 as Integer) in Integer
+
+        assert (sheet.A4 as Date) in java.util.Date
+        assert (sheet.A4 as Date) as String == "Thu Oct 22 00:00:00 JST 2009"
+
+        assert (sheet.A5 as Boolean) == true
+        assert (sheet.A6 as Boolean) == false
+    }
+
+    //void testRange() throws Exception {
+    //    def book = GExcel.load(new File("sample.xls"))
+    //    def sheet = book[0]
+    //    assert sheet[A1..B5]
+    //    sheet[A1..B5].filledBy 0
+    //    assert sheet[A1..B5]
+    //}
 }
