fbd05b4c0a8ef1d4f53ab449b901610e85ed73eb
frepe2013 <needvitneedfgt@gmail.com>
2014-06-19 02:35:09 +0900

    tab to space convert.

--- src/test/groovy/org/jggug/kobo/gexcelapi/GExcelEditTest.groovy
+++ src/test/groovy/org/jggug/kobo/gexcelapi/GExcelEditTest.groovy
@@ -8,59 +8,59 @@ import groovy.util.GroovyTestCase
 
 class GExcelEditTest extends GroovyTestCase {
 
-	def sampleFile = "build/resources/test/tasklist.xls"
-	def outputPath = "output.xls"
-	File outputFile
-	Workbook book
-	Sheet sheet
+    def sampleFile = "build/resources/test/tasklist.xls"
+    def outputPath = "output.xls"
+    File outputFile
+    Workbook book
+    Sheet sheet
 
-	void setUp() throws Exception {
-		book = GExcel.open(sampleFile)
-		sheet = book[0]
-		outputFile = new File(outputPath)
-	}
+    void setUp() throws Exception {
+        book = GExcel.open(sampleFile)
+        sheet = book[0]
+        outputFile = new File(outputPath)
+    }
 
-	void tearDown() {
-		book = null
-		sheet = null
-		//outputFile.delete()
-	}
+    void tearDown() {
+        book = null
+        sheet = null
+        outputFile.delete()
+    }
 
-	void testWrite() {
-		outputFile.withOutputStream { book.write(it) }
-		Workbook targetBook = GExcel.open(outputFile)
-		Sheet targetSheet = book[0]
+    void testWrite() {
+        outputFile.withOutputStream { book.write(it) }
+        Workbook targetBook = GExcel.open(outputFile)
+        Sheet targetSheet = book[0]
 
-		assert outputFile.exists()
-		assert sheet.A1 == targetSheet.A1
-		assert sheet.A2 == targetSheet.A2
-		assert sheet.B1 == targetSheet.B1
-		assert sheet.B2 == targetSheet.B2
-	}
+        assert outputFile.exists()
+        assert sheet.A1 == targetSheet.A1
+        assert sheet.A2 == targetSheet.A2
+        assert sheet.B1 == targetSheet.B1
+        assert sheet.B2 == targetSheet.B2
+    }
 
-	void testFindEmptyRow1() {
-		Row emptyRow = sheet.findEmptyRow('A2');
-		assert emptyRow != null
-		assert emptyRow.rowNum == 10 //　Rowクラスの行番号は0 始まり
-		assert emptyRow.getCell(0)?.value == null
-	}
+    void testFindEmptyRow1() {
+        Row emptyRow = sheet.findEmptyRow('A2');
+        assert emptyRow != null
+        assert emptyRow.rowNum == 10 //　Rowクラスの行番号は0 始まり
+        assert emptyRow.getCell(0)?.value == null
+    }
 
-	void testFindEmptyRow2() {
-		Row emptyRow = sheet.findEmptyRow('B3');
-		assert emptyRow != null
-		assert emptyRow.rowNum == 8 //　Rowクラスの行番号は0 始まり
-		assert emptyRow.getCell(1)?.value == null
-	}
+    void testFindEmptyRow2() {
+        Row emptyRow = sheet.findEmptyRow('B3');
+        assert emptyRow != null
+        assert emptyRow.rowNum == 8 //　Rowクラスの行番号は0 始まり
+        assert emptyRow.getCell(1)?.value == null
+    }
 
-	void testAddRow() {
-		Row emptyRow = sheet.findEmptyRow('A2');
-		emptyRow.createCell(0).value = "100"
-		emptyRow.createCell(1).value = "test"
+    void testAddRow() {
+        Row emptyRow = sheet.findEmptyRow('A2');
+        emptyRow.createCell(0).value = "100"
+        emptyRow.createCell(1).value = "test"
 
-		outputFile.withOutputStream { book.write(it) }
-		Workbook targetBook = GExcel.open(outputFile)
-		Sheet targetSheet = targetBook[0]
-		assert targetSheet.A11.value == "100"
-		assert targetSheet.B11.value == "test"
-	}
+        outputFile.withOutputStream { book.write(it) }
+        Workbook targetBook = GExcel.open(outputFile)
+        Sheet targetSheet = targetBook[0]
+        assert targetSheet.A11.value == "100"
+        assert targetSheet.B11.value == "test"
+    }
 }
